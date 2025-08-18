13a5c35aede3ea2c72726c27dbe849954f979685
IriPulenta07 <ipulenta@mail.austral.edu.ar>
2025-08-18 12:18:57 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print((f"{first_name} {last_name}").lower())
+    print((f"{first_name} {last_name}").title())
+    print((f"{first_name} {last_name}").upper())
+    print(f" \t {first_name} {last_name}")
